018ccb0a6b463a875a00fdb8da23cfcb81c3c2da
Ashishrock9394 <ashishkumar9394@gmail.com>
2026-02-02 16:20:26 +0530

    update:

--- app/Services/FitnessService.php
+++ app/Services/FitnessService.php
@@ -27,8 +27,6 @@ class FitnessService
         $studentAge = $dob->age;
         $gender = strtolower($student->gender) === 'male' ? 'Boys' : 'Girls';
 
-
-
         try {
             $result = DB::selectOne("
                 SELECT get_fitness_level(:skill_report_id, :age, :gender, :score, :criteria) AS level
